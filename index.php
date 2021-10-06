afcf935f94a0d67b186b80c7c74e11eb2a903712
Elmar Gerrits <97059019@st.deltion.nl>
2021-10-06 09:53:13 +0200

    phpopdracht 2.2 done

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+    if(isset($_POST["submit"])){
+    $naam = $_POST["naam"];
+    $land = $_POST["land"];
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,13 +18,13 @@
 Naam: <input type="text" name="naam" placeholder="Uw naam" required>
 <br>
 Land: <select name="land">
-        <option value="keuze" selected >Maak uw Keuze!</option>
-        <option value="NL">Nederland</option>
-        <option value="DE">Duitsland</option>
-        <option value="EN">Engeland</option>
-        <option value="IT">Italie</option>
-        <option value="FR">Frankrijk</option>
-        <option value="SP">Spanje</option>
+        <option value="keuze" <?php if($land=='') echo 'selected';?> >Maak uw Keuze!</option>
+        <option value="NL" <?php if($land=='NL') echo 'selected';?>>Nederland</option>
+        <option value="DE" <?php if($land=='DE') echo 'selected';?>>Duitsland</option>
+        <option value="EN" <?php if($land=='EN') echo 'selected';?>>Engeland</option>
+        <option value="IT" <?php if($land=='IT') echo 'selected';?>>Italie</option>
+        <option value="FR"<?php if($land=='FR') echo 'selected';?>>Frankrijk</option>
+        <option value="SP"<?php if($land=='SP') echo 'selected';?>>Spanje</option>
 </select>
 <br>
 <input type="submit" name="submit" value="Gegevens versturen" />
@@ -25,39 +32,75 @@ Land: <select name="land">
     </form>
     
     <?php
+    if(isset($_POST["submit"])){
 
-if(isset($_POST["submit"])){
-     $naam = $_POST["naam"];
-     $land = $_POST["land"]; 
-
-     if($land=='NL') {
+    switch ($land) {
+        case 'NL':
             echo 'Goedemorgen ' .$naam;
-     }
+            break;
 
-     elseif($land=='DE') {
-            echo 'Guten Morgen ' .$naam;
-     }
+    
+        case 'DE':
+            echo 'Gutenmorgen ' .$naam;
+            break;
 
-     elseif($land=='EN') {
-            echo 'Good morning ' .$naam;
-     }
+        case 'EN':
+            echo 'Good Morning ' .$naam;
+            break;
 
-     elseif($land=='IT') {
-            echo 'italiaanse tekst ' .$naam;
-     }
+        case 'IT':
+            echo 'Italiaans ' .$naam;
+            break;
 
-     elseif($land=='SP') {
-            echo 'spaanse tekst ' .$naam;
-     }
+        case 'FR':
+            echo 'Frans ' .$naam;
+            break;
 
-     elseif($land=='FR') {
-            echo 'franse tekst ' .$naam;
-     }
-
-     elseif($land='keuze') {
-         echo 'U heeft geen land geselecteerd';
-     } 
+        case 'SP':
+            echo 'Spaans ' .$naam;
+            break;
+        
+        default:
+            Echo 'U heeft geen land geselecteerd';
+            break;
     }
+}
+        
+        
+    
+// if(isset($_POST["submit"])){
+    // $naam = $_POST["naam"];
+    // $land = $_POST["land"]; 
+
+    // if($land=='NL') {
+        //    echo 'Goedemorgen ' .$naam;
+    // }
+
+    // elseif($land=='DE') {
+        //    echo 'Guten Morgen ' .$naam;
+    // }
+
+    // elseif($land=='EN') {
+        //    echo 'Good morning ' .$naam;
+    // }
+
+    // elseif($land=='IT') {
+        //    echo 'italiaanse tekst ' .$naam;
+    // }
+
+    // elseif($land=='SP') {
+        //    echo 'spaanse tekst ' .$naam;
+    // }
+
+    // elseif($land=='FR') {
+        //    echo 'franse tekst ' .$naam;
+    // }
+
+    // elseif($land='keuze') {
+        // echo 'U heeft geen land geselecteerd';
+    // } 
+//    }
+
 
         ?>
     
